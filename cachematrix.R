b85c5cd6f4bf13ee5e08ae14140e953dab955c5a
jamsheedc11 <75209312+jamsheedc11@users.noreply.github.com>
2020-11-29 15:17:58 +0000

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+# The makeCacheMatrix will make a matrix that will then cache its inverse
+# The argument 'input' is the initial matrix input from the user
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(input = matrix()) {
+      inversematrix<- NULL                    # creates an 'inverse' object and sets it to NULL for now
+      set<- function(y)   {               # function to set the value of the matrix
+      input<<- y
+      inversematrix<<- NULL
+      }
+      
+      get<- function() {input}               # function to get the value of the matrix
+      setinversematrix<- function(inverse) {inversematrix<<- inverse}    # setting the value of the inverse
+      getinversematrix<- function() {inversematrix}               # getting the value of the inverse
+            list(set=set, get=get,                          #return a list with the inverse of the matrix
+                 setinversematrix= setinversematrix,
+                 getinversematrix= getinversematrix)
 }
 
 
-## Write a short comment describing this function
+# The cacheSolve function calculates the inverse of the matrix created by makeCacheMatrix.
+# If the inverse of the matrix has been calculated, it will get this from the cache
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(input, ...) {
+      inversematrix <- input$getinversematrix()  # returns a matrix that is the inverse of the inputted matrix
+      if (!is.null(inversematrix)) {               # if inverse has already been calculated
+            message("getting cached data")
+            return(inversematrix)     # return the cached value of the inverse of the original matrix
+      }
+      newmat<- input$getinversematrix()    # otherwise compute the inverse and set its value in the cache
+      inversematrix<- solve(newmat,...)
+      input$setinversematrix(inversematrix)  #setting the value of the inverse in the cache
+      inversematrix                # return the initial matrix
 }
